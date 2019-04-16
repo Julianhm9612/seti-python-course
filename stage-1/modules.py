6751aa46199784418f5d0d32b6128c71f22e8d4e
JULIAN HENAO MARIN <julianhenao@SETIAD.COM.CO>
2019-04-16 17:52:31 -0500

    Add own modules and third party modules

--- stage-1/modules.py
+++ stage-1/modules.py
@@ -1,9 +1,22 @@
 # own modules
+import mMath
+from mMath import substract
+
+mMath.sum(1, 2)
+substract(3, 1)
 
 # third party modules
 # pip https://pypi.org/
+# pip install colorama
+from colorama import Fore, Style, init
+
+init(convert=True)
+print(Fore.RED + 'Hello World!' + Style.RESET_ALL)
 
 # python modules
 import datetime
+from datetime import timedelta, date
 
 print(datetime.date.today())
+print(timedelta(minutes=70)) # minutes to hours
+print(date.today())
